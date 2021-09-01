f6a14c5e171c6b0861d2ac8d43fc14a70f4815b1
prasanna-12 <78677332+prasanna-12@users.noreply.github.com>
2021-09-01 23:57:57 +0530

    Add files via upload

---
+++ BookMovieApp/Final/src/common/header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavLink, withRouter} from 'react-router-dom'
+import {ReactComponent as Logo} from '../../assets/logo.svg';
+
+import './header.css';
+const Header = ({history,isLogged}) =>{
+    const handleClick=() =>{
+        history.push('/')
+        isLogged(false)
+    }
+    return(
+        <nav>
+            <div className='div-header'>
+                <div className='div-svg' onClick={() => history.push('/')}>
+                    <Logo/>
+                </div>
+                <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+                    <NavLink exact to='/' activeClassName='active'><Home className='div-svg'/></NavLink>
+                    <NavLink exact to='/explore' activeClassName='active'><Explore className='div-svg'/></NavLink>
+                    <button className='button-header' onClick={handleClick}>Log out</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default withRouter(Header);
